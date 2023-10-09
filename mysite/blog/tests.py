47edaa6bb2027531a2e7719980e519df95e81f47
Eduardo Serrano <axenedu@gmail.com>
2023-10-09 11:48:50 +0200

    Test para probar el formulario

--- mysite/blog/tests.py
+++ mysite/blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+#Create a test to check PostForm contains the fields specified in the tutorial
+from .forms import PostForm
+
+class PostFormTest(TestCase):
+
+    def test_postform_fields(self):
+        form = PostForm()
+        expected = ['title', 'text']
+        actual = list(form.fields)
+        self.assertSequenceEqual(expected, actual)
+
+    #Create a test to check PostForm is a ModelForm and uses the Post model
+    def test_postform_inherits_from_model_form(self):
+        form = PostForm()
+        self.assertEqual(form.__class__.__bases__[0].__name__, 'ModelForm')
+        #check thag parent class package is django.forms.models
+        self.assertEqual(form.__class__.__bases__[0].__module__, 'django.forms.models')
+    def test_postform_model(self):
+        form = PostForm()
+        self.assertEqual(form.Meta.model.__name__, 'Post')
